eff2a3d2361a002dfb7750d29a569c0f232df21f
AndelrahmanAbdelmogeth <abdelrahmanabdelmougeth@gmaill.com>
2024-10-25 13:58:42 +0300

    create redux slice

---
+++ src/features/tasks/taskSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = [];
+
+const taskSlice = createSlice({
+  name: 'tasks',
+  initialState,
+  reducers: {
+    addTask: (state, action) => {
+      state.push(action.payload);
+    },
+    editTask: (state, action) => {
+      const { id, updates } = action.payload;
+      const index = state.findIndex(task => task.id === id);
+      if (index !== -1) {
+        state[index] = { ...state[index], ...updates };
+      }
+    },
+    deleteTask: (state, action) => {
+      return state.filter(task => task.id !== action.payload);
+    },
+  },
+});
+
+export const { addTask, editTask, deleteTask } = taskSlice.actions;
+export default taskSlice.reducer;
